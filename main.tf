d4eb442b1a5521bc21d128f3601c96d6cc3ef568
naman <namanagrawal6222@gmail.com>
2022-02-15 20:22:19 +0530

    added security group

--- main.tf
+++ main.tf
@@ -6,38 +6,74 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
+//variable instance_type {}
+// variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
-        Name: "${var.env_prefix}-vpc"
+        Name = "${var.env_prefix}-vpc"
     }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id 
+    vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
-        Name: "${var.env_prefix}-subnet-1"
-    }
-}
-
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags {
-        Name: "${var.env_prefix}-route-table"
+        Name = "${var.env_prefix}-subnet-1"
     }
 }
 
 resource "aws_internet_gateway" "myapp-igw" {
     vpc_id = aws_vpc.myapp-vpc.id
-    tags {
-        Name: "${var.env_prefix}-igw"
+    tags = {
+        Name = "${var.env_prefix}-igw"
+    }
+}
+
+// using default route table
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name = "${var.env_prefix}-main-rtb"
+    }
+}
+
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    // for incoming traffic
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }    
+
+    // for outgoing traffic
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name = "${var.env_prefix}-default"
     }
 }
